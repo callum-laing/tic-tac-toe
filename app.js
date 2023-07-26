df6863ae0c6ba2bf957cc3494c84af3605b443f0
Callum Laing <callum.laing91@hotmail.co.uk>
2023-07-26 22:17:18 +0100

    added functionality to make game playable

--- app.js
+++ app.js
@@ -20,3 +20,60 @@ function checkWinner(player) {
     [2, 4, 6], // Diagonals
   ];
 }
+for (const combination of winningCombination) {
+  if (
+    board[combination[0]] === player &&
+    board[combination[1]] === player &&
+    board[combination[2]] === player
+  ) {
+    return true;
+  }
+}
+return false;
+
+function isBoardFull() {
+  return board.every((cell) => cell !== "");
+}
+
+function makeMove(index) {
+  if (board[index] === "") {
+    board[index] = currentPlayer;
+    document.getElementById("cell-" + index).textContent = currentPlayer;
+    if (checkWinner(currentPlayer)) {
+      alert(currentPlayer + " wins!");
+      resetBoard();
+    } else if (isBoardFull()) {
+      alert("It's a tie!");
+      resetBoard();
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      if (currentPlayer === "O") {
+        computerMove();
+      }
+    }
+  }
+}
+
+function computerMove() {
+  const emptyCells = board.reduce((acc, cell, index) => {
+    if (cell === "") {
+      acc.push(index);
+    }
+    return acc;
+  }, []);
+
+  const randomIndex = Math.floor(Math.random() * emptyCells.length);
+  makeMove(emptyCells[randomIndex]);
+}
+
+function resetBoard() {
+  for (let i = 0; i < 9; i++) {
+    board[i] = "";
+    document.getElementById("cell-" + i).textContent = "";
+  }
+  currentPlayer = "X";
+}
+
+document.querySelectorAll("td").forEach((cell, index) => {
+  cell.addEventListener("click", () => makeMove(index));
+});
